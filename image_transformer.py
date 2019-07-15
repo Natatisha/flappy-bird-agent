2e89d48749c2145e923d85dc072c74fb7291910c
Natatisha <natatisha94@gmail.com>
2019-07-15 09:54:51 -0700

    image transformer experiment

--- image_transformer.py
+++ image_transformer.py
@@ -4,11 +4,12 @@ import tensorflow as tf
 class ImageTransformer:
     def __init__(self, origin_shape=(512, 288, 3), out_shape=(84, 84), crop_boundaries=(0, 50, 400, 238)):
         with tf.variable_scope("image_transformer"):
-            self.input_img = tf.placeholder(shape=origin_shape, dtype=tf.int8)
+            self.input_img = tf.placeholder(shape=origin_shape, dtype=tf.int32)
             self.output = tf.image.rgb_to_grayscale(self.input_img)
             self.output = tf.image.crop_to_bounding_box(self.output, *crop_boundaries)
             self.output = tf.image.resize_images(self.output, size=out_shape,
                                                  method=tf.image.ResizeMethod.NEAREST_NEIGHBOR)
+            self.output = tf.clip_by_value(self.output, 0, 90)
             self.output = tf.squeeze(self.output)
 
     def transform(self, image, sess=None):
