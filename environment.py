6ab3aee86ca59170c6caf2178ee4d2985237b78d
Natatisha <natatisha94@gmail.com>
2019-08-02 14:28:18 -0700

    reward experients

--- environment.py
+++ environment.py
@@ -61,11 +61,13 @@ class FlappyBirdWrapper(object):
 
     def _process_reward(self, state, reward):
         if not self.screen_out:
-            dist = self._calc_dist(state[0], state[3], state[2])
             if reward == 0:
+                dist = self._calc_dist(state[0], state[3], state[2])
                 reward = 0.99 ** dist
             elif reward < 0:
                 reward = -300
+            else:
+                reward = 5
         return reward
 
     def _process_action(self, action):
@@ -92,3 +94,12 @@ class FlappyBirdWrapper(object):
 
     def close(self):
         pass  # if we'll decide to change environment, for example to OpenAI gym, we'll need this function
+
+#
+# import matplotlib.pyplot as plt
+#
+# if __name__ == "__main__":
+#     X = np.arange(50, 310, 1.)
+#     calc = lambda dist: 0.99**dist
+#     plt.plot(X, calc(X))
+#     plt.show()
